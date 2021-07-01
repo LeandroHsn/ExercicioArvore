9cd5aeda64415faff8ad28d48327ff53c4ee462a
LeandroHenrickSilvaNunes <leandroh@kyros.com.br>
2021-06-30 23:06:33 -0300

    Primeiro commit

---
+++ src/App.java
@@ -0,0 +1,16 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        System.out.println("\nExercício 1\n");
+
+        System.out.println("Pré-ordem: 8, 9, 11, 15, 19, 20, 21, 7, 3, 2, 1, 5, 6, 4, 13, 14, 10, 12, 17, 16, 18");
+
+        System.out.println("In-ordem: 19, 15, 11, 21, 20, 9, 2, 3, 7, 1, 8, 13, 4, 6, 14, 10, 5, 16, 14, 12, 18");
+
+        System.out.println("Pós-ordem: 19, 15, 21, 20, 11, 2, 3, 1, 7, 9, 13, 4, 10, 14, 6, 16, 14, 18, 12, 5, 8");
+
+        System.out.println("\nExercício 2");
+
+        System.out.println("\nExercício 3");
+    }
+}
