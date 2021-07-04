b284fc4a9d939b4dfae25d1a76545baac8bf7d1b
LeandroHenrickSilvaNunes <leandroh@kyros.com.br>
2021-07-04 11:30:32 -0300

    Adicionando outros exercícios

--- src/App.java
+++ src/App.java
@@ -1,5 +1,9 @@
+import java.util.Random;
+
 public class App {
     public static void main(String[] args) throws Exception {
+
+        // *********************** EXERCÍCIO 1 ***********************
 
         System.out.println("\nExercício 1\n");
 
@@ -9,8 +13,56 @@ public class App {
 
         System.out.println("Pós-ordem: 19, 15, 21, 20, 11, 2, 3, 1, 7, 9, 13, 4, 10, 14, 6, 16, 14, 18, 12, 5, 8");
 
-        System.out.println("\nExercício 2");
 
-        System.out.println("\nExercício 3");
+        // *********************** EXERCÍCIO 2 ***********************
+
+
+        System.out.println("\nExercício 2\n");
+
+        Arvore arvore = new Arvore();
+        int valor;
+        Random rand = new Random();
+        int valorBusca = 0;
+
+        System.out.print("Os valores da nossa Arvore Binária são: ");
+        for (int i = 0; i < 10; i++) {
+            valor = rand.nextInt(100);
+            System.out.print(valor + " ");
+            arvore.insereNo(valor);
+            if (i == 5) {
+                valorBusca = valor;
+            }
+        }
+
+        System.out.print("\n\nPreOrdem Transversal: ");
+        arvore.preOrdemRecursivoTransversal();
+
+        System.out.print("\n\nInOrdem Transversal: ");
+        arvore.inOrdemRecursivoTransversal();
+
+        System.out.print("\n\nPosOrdem Transversal: ");
+        arvore.posOrdemRecursivoTransversal();
+
+
+        // *********************** EXERCÍCIO 3 ***********************
+
+
+        System.out.println("\n\nExercício 3");
+
+        No aux = arvore.buscar(valorBusca);
+        arvore.buscar(valorBusca);
+        if (aux != null) {
+            System.out.printf("\nO valor %d está na arvore binária\n", aux.valor);
+        } else {
+            System.out.println("\nO valor não está na arvore binária\n");
+        }
+
+        No aux2 = arvore.buscar(101);
+        arvore.buscar(101);
+        if (aux2 != null) {
+            System.out.printf("\nO valor %d está na arvore binária\n", aux2.valor);
+        } else {
+            System.out.println("\nO valor não está na arvore binária\n");
+        }
     }
 }
---
+++ src/No.java
@@ -0,0 +1,34 @@
+public class No {
+    public No noEsquerdo;
+    public int valor;// valor que arvore vai armazenar
+    public No noDireito;
+
+    // metodo construtor da classe Nó
+    public No(int valor) {
+        this.valor = valor;
+        noEsquerdo = null;
+        noDireito = null;
+    }
+
+    // localiza o ponto de inserção e insere o novo nó;
+    // ignorar valores duplicados;
+    public void insereNo(int valorIsercao) {
+        // insere na subárvore esquerda
+        if (valorIsercao < this.valor) {
+            // insere um novo Nó
+            if (this.noEsquerdo == null) {
+                this.noEsquerdo = new No(valorIsercao);
+            } else {// contiua percorrendo subárvore esquerda
+                this.noEsquerdo.insereNo(valorIsercao);
+            }
+            // insere na subárvore direita
+        } else if (valorIsercao > this.valor) {
+            // insere um novo Nó
+            if (this.noDireito == null) {
+                this.noDireito = new No(valorIsercao);
+            } else {// contiua percorrendo subárvore direita
+                this.noDireito.insereNo(valorIsercao);
+            }
+        }
+    }
+}
